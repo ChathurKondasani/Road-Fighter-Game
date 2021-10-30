20b5f990653f41966f3ef43d7696f7df7a2b7f3f
ChathurKondasani <87412487+ChathurKondasani@users.noreply.github.com>
2021-10-30 17:27:43 +0530

    Add files via upload

---
+++ RoadFighter.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+#include<dos.h>
+#include<window.h>
+#include<time.h>
+
+#define SCREEN_WIDTH 100
+#define SCREEN_WIDTH 30
+#define WIN_WIDTH 80
+
+using namespace std;
+
+Handel console = GetStdHandel(STD_OUTPUT_HANDEL);
+COORD CursorPosition:
+
+int enemyY[5];
+int enemyX[5];
+int enemyFlag[3];
+char car[5][5] = (' ',' ')
